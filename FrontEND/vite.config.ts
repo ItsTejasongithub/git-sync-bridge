d43c7ba5f712c388ce56c3fda0da90bf5efcae8e
Tejas On Github <tejas.kandi@10xtechclub.com>
2026-02-09 13:27:26 +0530

    certificate

--- FrontEND/vite.config.ts
+++ FrontEND/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -10,18 +8,10 @@ export default defineConfig({
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,
     strictPort: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../BackEND/certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../BackEND/certs/cert.pem')),
-    },
   },
   preview: {
     host: '0.0.0.0',
     port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../BackEND/certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../BackEND/certs/cert.pem')),
-    },
   },
   build: {
     minify: 'terser',
